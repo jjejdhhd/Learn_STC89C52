06ee32a00096a5a5e3a288f4c8dc70c5c355a3e9
百慕亮 <46186176+jjejdhhd@users.noreply.github.com>
2023-01-30 12:56:05 +0800

    Add files via upload

---
+++ 05_定时器与中断/5-1按键控制流水灯-定时器/PushKey.h
@@ -0,0 +1,19 @@
+#ifndef __PUSHKEY_H__
+#define __PUSHKEY_H__
+
+#include "Delay.h"
+/**
+  * @brief :检测按下了哪个按键开关
+  * @param :无
+  * @retval :输出按键开关编号1~4，不按返回0，松开触发
+ */
+unsigned char PushKey(){
+  unsigned char key = 0;
+  if(!P3_1){Delay(10);while(!P3_1);Delay(10);key=1;}
+  else if(!P3_0){Delay(10);while(!P3_1);Delay(10);key=2;}
+  else if(!P3_2){Delay(10);while(!P3_1);Delay(10);key=3;}
+  else if(!P3_3){Delay(10);while(!P3_1);Delay(10);key=4;}
+  return key;
+}
+
+#endif
