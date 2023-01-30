cf3a6b0fbcaad2bc8ceb8c3eb1386b794ac86485
百慕亮 <46186176+jjejdhhd@users.noreply.github.com>
2023-01-30 13:19:11 +0800

    Delete 8_DS1302实时时钟 directory

--- 8_DS1302实时时钟/实验1_实时时钟/DS1302.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DS1302_H__
-#define __DS1302_H__
-
-void DS1302_Init(void);//初始化DS1302
-unsigned char DS1302_ReadByte(unsigned char read_comd);//从DS1302中读出数据
-void DS1302_WriteByte(unsigned char wri_comd,wri_byte);//向DS1302中写入数据
-void DS1302_ReadAll(unsigned char *info);//读出所有信息
-
-#endif
